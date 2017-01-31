7bad8249d64070d196ca38523fcc6aa0a4ac2f0f
Daniel Santos <dsantosp12@gmail.com>
2017-01-31 16:05:43 -0500

    Added link to repl definition

--- 01-Python Basics/01-The Shell/The_Shell.py
+++ 01-Python Basics/01-The Shell/The_Shell.py
@@ -6,5 +6,8 @@
 #   Here we are going to see how to use the Python shell.   #
 #   No code is necesarry here. We will be looking at the    #
 #   print() function and how we can test anything using     #
-#    the shell.                                             # 
+#    the shell.                                             #
+#                                                           #
+#   https://en.wikipedia.org/wiki/Read–eval–print_loop      #
+#                                                           #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
